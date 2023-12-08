546c65e3dbc6cd146194ee2150d9554d7537002e
Douaesb <douae.sb411@gmail.com>
2023-12-08 14:47:01 +0100

    solution no check

--- dashboardp.php
+++ dashboardp.php
@@ -43,6 +43,10 @@ if (!isset($_SESSION["email"])) {
                     </li>
 
                     <li>
+                        <a href="dashboardpStats.php" class="mr-40 py-2 px-3 text-black rounded md:bg-transparent md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Stats</a>
+                    </li>
+
+                    <li>
                         <a href="logout.php" class="block py-2 px-3 text-pink-500 rounded hover:bg-blue-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Deconnexion</a>
                     </li>
                 </ul>
