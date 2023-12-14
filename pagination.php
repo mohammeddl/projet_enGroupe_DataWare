a020053363d28672f144acc6ac8f503028ffee69
mohammeddl <mohammed_daali2@um5.ac.ma>
2023-12-14 14:07:43 +0100

    fix like and dislike

--- pagination.php
+++ pagination.php
@@ -24,6 +24,7 @@ function dd($data)
 
 
 $email =$_SESSION['email'];
+$current_user_id = $_SESSION['id'];
 $role = "SELECT role FROM utilisateur WHERE email = '$email'";
 $email2 = mysqli_query($conn, $role);
 $rowScrum = mysqli_fetch_array($email2);
@@ -32,15 +33,28 @@ $output = "";
 $row = mysqli_num_rows($fetch_query);
 
 
+
+
 if ($row > 0) {
     $i = 0;
     while ($res = mysqli_fetch_array($fetch_query)) {
         if ($res['archive_qst'] == 0) {
             $idqst = $res['id_qst'];
             $iduser = $res['id_user'];
-            $sql2 = "SELECT likee, dislike FROM reactions WHERE id_qst = $idqst";
-            $result2 = mysqli_query($conn, $sql2);
-            $res2 = mysqli_fetch_array($result2);
+            // $sql2 = "SELECT likee, id_user, dislike FROM reactions WHERE id_qst = $idqst";
+            // $result2 = mysqli_query($conn, $sql2);
+            // $res2 = mysqli_fetch_array($result2);
+            $sql2 = "SELECT * FROM reactions WHERE id_qst = $idqst AND id_user = $current_user_id";
+            $result2 =mysqli_query($conn, $sql2);
+            $res2 = mysqli_fetch_assoc($result2);
+
+            $likesNumberQuery = "SELECT COUNT(likee) FROM reactions WHERE id_qst = $idqst";
+            $rowCountLike = mysqli_query($conn,$likesNumberQuery );
+            $resultCountLike = mysqli_fetch_assoc($rowCountLike);
+
+            $dislikeQuery = "SELECT COUNT(dislike) FROM reactions WHERE id_qst = $idqst";
+            $rowCountDislike = mysqli_query($conn,$dislikeQuery );
+            $resultCountDislike = mysqli_fetch_assoc($rowCountDislike);
             $output .= " 
         <div>
 
@@ -60,7 +74,7 @@ if ($row > 0) {
 
                             <form class='flex gap-5' action='' method='get'>";
             $output .= "<input type='hidden' name='id_qst' id='id_qst$i' value='$idqst'>";
-            $output .= "<input type='hidden' name='id_user' id='id_user' value='$iduser'>";
+            $output .= "<input type='hidden' name='id_user' id='id_user' value='$current_user_id'>";
 
 
 
@@ -87,25 +101,36 @@ if ($row > 0) {
                             </div>
                         </div>
                         <div class='grid grid-cols-2 gap-4 md:flex md:items-center mt-4 mb-10 md:ml-12'>
-                            <div class='flex md:items-center bg-gray-100  rounded-lg px-3 py-2 cursor-pointer group likebtndiv'  >
+                            <div class='flex md:items-center bg-gray-100  rounded-lg px-3 py-2 cursor-pointer group likebtndiv ";
+                            if(!empty($res2)){
+            $output .= $res2['likee'] != null ? "bg-green-400" : "" ;
+                            }
+            $output .= "'  >
                                 <svg class='mr-2 fill-current text-primary-black group-hover:text-primary' width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
                                     <path d='M17.4606 10.0435C17.8096 9.59094 18 9 18 8.27817C18 6.9537 16.8724 5.72685 15.4432 5.72685H12.977C13.2835 5.11625 13.7045 4.35888 13.7045 3.27234C13.7046 1.21894 12.8854 0 10.7898 0C9.73529 0 9.34397 1.33305 9.14347 2.36348C9.02369 2.97911 8.91053 3.56063 8.56252 3.90864C7.73388 4.7373 6.46875 6.75 5.70874 7.15869C5.63179 7.19111 5.53345 7.21663 5.42637 7.23684C5.22341 6.9428 4.88429 6.75 4.5 6.75H1.125C0.503684 6.75 0 7.25368 0 7.875V16.875C0 17.4963 0.503684 18 1.125 18H4.5C5.12132 18 5.625 17.4963 5.625 16.875V16.5677C6.7674 16.5677 9.16478 18.0002 11.8637 17.9995C12.0572 17.9996 13.1873 18.0006 13.3055 17.9995C15.3896 18 16.5489 16.7379 16.4814 14.8427C17.0132 14.2195 17.2737 13.3192 17.1221 12.4836C17.56 11.7971 17.6539 10.8288 17.4606 10.0435ZM1.125 16.875V7.875H4.5V16.875H1.125ZM16.0318 9.7155C16.5938 10.125 16.5938 11.8125 15.8347 12.1998C16.3101 13 15.8823 14.0707 15.3069 14.3794C15.5984 16.2265 14.6403 16.8616 13.2955 16.8745C13.1791 16.8756 11.986 16.8745 11.8637 16.8745C9.29978 16.8745 7.12666 15.4427 5.62504 15.4427V8.28369C6.95071 8.28369 8.16701 5.89521 9.35803 4.70415C10.4319 3.6303 10.0739 1.84054 10.7898 1.12465C12.5796 1.12465 12.5796 2.37329 12.5796 3.27238C12.5796 4.75559 11.5058 5.42007 11.5058 6.85188H15.4432C16.2423 6.85188 16.8715 7.5678 16.875 8.28369C16.8785 8.99958 16.5938 9.5625 16.0318 9.7155ZM3.65625 15.1875C3.65625 15.6535 3.2785 16.0312 2.8125 16.0312C2.3465 16.0312 1.96875 15.6535 1.96875 15.1875C1.96875 14.7215 2.3465 14.3438 2.8125 14.3438C3.2785 14.3438 3.65625 14.7215 3.65625 15.1875Z'></path>
                                 </svg>
 
                                 <span class='text-sm text-gray-800 likebtn'  >";
-            $output .= $res2['likee'] ? $res2['likee'] : 0;
-            $output .= "</span>
+                                
+                                $output .= $resultCountLike['COUNT(likee)'];
+                                
+                                $output .= "</span>
                             </div>
 
-                            <div class='flex md:items-center bg-gray-100  rounded-lg px-3 py-2 cursor-pointer group dislikebtndiv'  >
-                                <svg class='mr-2 fill-current text-primary-black group-hover:text-primary' width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
-                                    <g transform='rotate(180 9 9)'>
-                                        <path d='M17.4606 10.0435C17.8096 9.59094 18 9 18 8.27817C18 6.9537 16.8724 5.72685 15.4432 5.72685H12.977C13.2835 5.11625 13.7045 4.35888 13.7045 3.27234C13.7046 1.21894 12.8854 0 10.7898 0C9.73529 0 9.34397 1.33305 9.14347 2.36348C9.02369 2.97911 8.91053 3.56063 8.56252 3.90864C7.73388 4.7373 6.46875 6.75 5.70874 7.15869C5.63179 7.19111 5.53345 7.21663 5.42637 7.23684C5.22341 6.9428 4.88429 6.75 4.5 6.75H1.125C0.503684 6.75 0 7.25368 0 7.875V16.875C0 17.4963 0.503684 18 1.125 18H4.5C5.12132 18 5.625 17.4963 5.625 16.875V16.5677C6.7674 16.5677 9.16478 18.0002 11.8637 17.9995C12.0572 17.9996 13.1873 18.0006 13.3055 17.9995C15.3896 18 16.5489 16.7379 16.4814 14.8427C17.0132 14.2195 17.2737 13.3192 17.1221 12.4836C17.56 11.7971 17.6539 10.8288 17.4606 10.0435ZM1.125 16.875V7.875H4.5V16.875H1.125ZM16.0318 9.7155C16.5938 10.125 16.5938 11.8125 15.8347 12.1998C16.3101 13 15.8823 14.0707 15.3069 14.3794C15.5984 16.2265 14.6403 16.8616 13.2955 16.8745C13.1791 16.8756 11.986 16.8745 11.8637 16.8745C9.29978 16.8745 7.12666 15.4427 5.62504 15.4427V8.28369C6.95071 8.28369 8.16701 5.89521 9.35803 4.70415C10.4319 3.6303 10.0739 1.84054 10.7898 1.12465C12.5796 1.12465 12.5796 2.37329 12.5796 3.27238C12.5796 4.75559 11.5058 5.42007 11.5058 6.85188H15.4432C16.2423 6.85188 16.8715 7.5678 16.875 8.28369C16.8785 8.99958 16.5938 9.5625 16.0318 9.7155ZM3.65625 15.1875C3.65625 15.6535 3.2785 16.0312 2.8125 16.0312C2.3465 16.0312 1.96875 15.6535 1.96875 15.1875C1.96875 14.7215 2.3465 14.3438 2.8125 14.3438C3.2785 14.3438 3.65625 14.7215 3.65625 15.1875Z'></path>
-                                    </g>
-                                </svg>
-
-                                <span class='text-sm text-gray-800 dislikebtn'>";
-            $output .=  $res2['dislike'] ? $res2['dislike'] : 0;
+                            <div class='flex md:items-center bg-gray-100  rounded-lg px-3 py-2 cursor-pointer group dislikebtndiv ";
+                            if(!empty($res2)){
+                                $output .= $res2['dislike'] != null ? "bg-red-400" : "";
+                            }
+                            $output .= "'  >
+                            <svg class='mr-2 fill-current text-primary-black group-hover:text-primary' width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                            <g transform='rotate(180 9 9)'>
+                            <path d='M17.4606 10.0435C17.8096 9.59094 18 9 18 8.27817C18 6.9537 16.8724 5.72685 15.4432 5.72685H12.977C13.2835 5.11625 13.7045 4.35888 13.7045 3.27234C13.7046 1.21894 12.8854 0 10.7898 0C9.73529 0 9.34397 1.33305 9.14347 2.36348C9.02369 2.97911 8.91053 3.56063 8.56252 3.90864C7.73388 4.7373 6.46875 6.75 5.70874 7.15869C5.63179 7.19111 5.53345 7.21663 5.42637 7.23684C5.22341 6.9428 4.88429 6.75 4.5 6.75H1.125C0.503684 6.75 0 7.25368 0 7.875V16.875C0 17.4963 0.503684 18 1.125 18H4.5C5.12132 18 5.625 17.4963 5.625 16.875V16.5677C6.7674 16.5677 9.16478 18.0002 11.8637 17.9995C12.0572 17.9996 13.1873 18.0006 13.3055 17.9995C15.3896 18 16.5489 16.7379 16.4814 14.8427C17.0132 14.2195 17.2737 13.3192 17.1221 12.4836C17.56 11.7971 17.6539 10.8288 17.4606 10.0435ZM1.125 16.875V7.875H4.5V16.875H1.125ZM16.0318 9.7155C16.5938 10.125 16.5938 11.8125 15.8347 12.1998C16.3101 13 15.8823 14.0707 15.3069 14.3794C15.5984 16.2265 14.6403 16.8616 13.2955 16.8745C13.1791 16.8756 11.986 16.8745 11.8637 16.8745C9.29978 16.8745 7.12666 15.4427 5.62504 15.4427V8.28369C6.95071 8.28369 8.16701 5.89521 9.35803 4.70415C10.4319 3.6303 10.0739 1.84054 10.7898 1.12465C12.5796 1.12465 12.5796 2.37329 12.5796 3.27238C12.5796 4.75559 11.5058 5.42007 11.5058 6.85188H15.4432C16.2423 6.85188 16.8715 7.5678 16.875 8.28369C16.8785 8.99958 16.5938 9.5625 16.0318 9.7155ZM3.65625 15.1875C3.65625 15.6535 3.2785 16.0312 2.8125 16.0312C2.3465 16.0312 1.96875 15.6535 1.96875 15.1875C1.96875 14.7215 2.3465 14.3438 2.8125 14.3438C3.2785 14.3438 3.65625 14.7215 3.65625 15.1875Z'></path>
+                            </g>
+                            </svg>
+                            
+                            <span class='text-sm text-gray-800 dislikebtn'>";
+                            $output .= $resultCountDislike['COUNT(dislike)'];
+                                
             $output .= "</span>
                             </div>
 
@@ -255,9 +280,6 @@ if ($row > 0) {
 
 
 
-
-
-
 <?php
 $fetch_query_all = mysqli_query($conn, "select * from question");
 $total_page = ceil(mysqli_num_rows($fetch_query_all) / $limit_page);
@@ -282,7 +304,6 @@ echo $output;
 
     
     let clickedLikes = false;
-
     let clickedDislike = false;
 
     for (let i = 0; i < likebtn.length; i++) {
@@ -295,31 +316,22 @@ echo $output;
                 let likes = parseInt(likebtnint[i].innerText);
                 const xhr = new XMLHttpRequest();
                 
-                let insert = 'insert';
-                let update = 'update';
                 if(!clickedLikes){
                     
                     clickedLikes = true;
                 
                     likes++;
-                    
-                    console.log('incress : ',likes)
-                    xhr.open("GET", "reactions.php?qstid=" + id + "&likes=" + likes + "&iduser=" + iduser + "&update="+update, true);
-                    likebtn[i].style.backgroundColor = 'green';
+                    xhr.open("GET", "reactions.php?qstid=" + id  + "&iduser=" + iduser , true);
                 console.log("Liked!");
                 
-                }else{
-                    clickedLikes = false;
-                    console.log(clickedLikes)
-                    likes--;
-                    console.log('decresslike : ',likes)
-                    xhr.open("GET", "reactions.php?qstid=" + id + "&likes=" + likes + "&iduser=" + iduser + "&update="+update, true);
-                    likebtn[i].style.backgroundColor = 'gray';
-                console.log("Unliked!");
+                // }else{
+                //     clickedLikes = false;
+                //     console.log(clickedLikes)
+                //     likes--;
+                //     console.log('decresslike : ',likes)
+                //     xhr.open("GET", "reactions.php?qstid=" + id + "&likes=" + likes + "&iduser=" + iduser + "&update="+update, true);
+                // console.log("Unliked!");
                 
-                
-                
-
                 }
 
                 xhr.onload = function() {
@@ -344,15 +356,12 @@ echo $output;
                 let dislikes = parseInt(dislikebtnint[i].innerText);
                 const xhr = new XMLHttpRequest();
             
-                let insert = 'insert';
-                let update = 'update';
                 let dis = 'dislike';
                 if(!clickedDislike){
                     clickedDislike = true;
                     console.log(clickedDislike)
                     dislikes++;
-                    xhr.open("GET", "reactions.php?qstid=" + id + "&dislikes=" + dislikes+ "&iduser=" + iduser + "&insert="+insert +"&type="+dis, true);
-                    dislikebtn[i].style.backgroundColor = 'red';
+                    xhr.open("GET", "reactions.php?qstid=" + id +  "&iduser=" + iduser +"&type="+dis, true);
             console.log("Liked!");
                 }else{
                     clickedDislike = false;
@@ -360,7 +369,6 @@ echo $output;
                     
                     dislikes--;
                     xhr.open("GET", "reactions.php?qstid=" + id + "&dislikes=" + dislikes+ "&iduser=" + iduser + "&update="+update +"&type="+dis, true);
-                    dislikebtn[i].style.backgroundColor = 'gray';
             console.log("Unliked!");
                 }
 
--- reactions.php
+++ reactions.php
@@ -1,30 +1,30 @@
 <?php
 include('connection.php');
+session_start();
 $idqst = $_GET['qstid'];
 $iduser = $_GET['iduser'];
-$likes = $_GET['likes'];
-$dislikes = $_GET['dislikes'];
+// $likes = $_GET['likes']; => to remove 
+// $dislikes = $_GET['dislikes'];
 $type = $_GET['type'];
-// echo "idqst is $idqst";
-// echo "iduser is $iduser";
-// echo "likes is $likes";
+
+
+
+$sqlDelete = " DELETE FROM reactions WHERE id_user = $iduser AND id_qst = $idqst";
+mysqli_query($conn, $sqlDelete);
+
+
+
 
 if ($type) {
-    if (isset($_GET['insert']) && $_GET['insert'] === 'insert') {
-     
-        $sql = "UPDATE reactions set dislike = $dislikes where id_qst = $idqst";
-        mysqli_query($conn, $sql);
-    } elseif (isset($_GET['update']) && $_GET['update'] === 'update') {
-        $sql = "UPDATE reactions set dislike = $dislikes where id_qst = $idqst";
-        mysqli_query($conn, $sql);
-    }
-} else {
+    
+        $sqlDi="INSERT INTO reactions(id_qst,id_user,dislike) values($idqst,$iduser,1)";
+mysqli_query($conn, $sqlDi);
+    } else  {
+        $sql = "INSERT INTO reactions(id_qst,id_user,likee) values($idqst,$iduser,1)";
+mysqli_query($conn, $sql);
+    
 
-    if (isset($_GET['insert']) && $_GET['insert'] === 'insert') {
-        $sql = "INSERT INTO reactions(id_qst,id_user,likee) values($idqst,$iduser,$likes)";
-        mysqli_query($conn, $sql);
-    } elseif (isset($_GET['update']) && $_GET['update'] === 'update') {
-        $sql = "UPDATE reactions set likee = $likes where id_qst = $idqst";
-        mysqli_query($conn, $sql);
-    }
-}
+} 
+
+
+
